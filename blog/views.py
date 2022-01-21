d1efe505a59ce88105c8670de16d806247f1a321
Robert Coffie <rcoffie22@yahoo.com>
2022-01-21 16:15:35 +0000

    buidling search query with postgresql

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.views.generic import ListView 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail 
 from taggit.models import Tag
 from django.db.models import Count 
+from django.contrib.postgres.search import SearchVector 
+from blog.models import Post 
+
 
 # Create your views here.
 def post_list(request, tag_slug=None):
@@ -72,7 +75,17 @@ def post_share(request, post_id):
   return render(request, 'blog/post/share.html',{'post':post,'form':form,'sent':sent})
    
 
-
-
+def post_search(request):
+  form = SearchForm()
+  query = None 
+  results = []
+  if 'query' in request.GET:
+    form = SearchForm(request.GET)
+    if form.is_valid():
+      query = form.cleaned_data['query']
+      results = Post.published.annotate(
+        search=SearchVector('title','body'),
+      ).filter(search=query)
+  return render(request,'blog/post/search.html',{'form':form,'query':query,'results':results})
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog.apps.BlogConfig',
-    'taggit'
+    'taggit',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -75,12 +80,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER':'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': 'nana'
+    }
 }
+
 
 
 # Password validation
